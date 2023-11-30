341e6286aae0fb8f1c7d8af93624b74dafb63c4e
IAVGROUP\s01markw <sascha.markwardt@iav.de>
2023-11-30 09:57:16 +0100

    add run.py

--- components/run.py
+++ components/run.py
@@ -6,26 +6,36 @@ import offender_detector
 import victim_detector
 import guardian_angel
 import coordinate_system_transformer
+import RemoteVehicle
 
 
-thraed_guardian_angel = threading.Thread(target=guardian_angel.run)
+# python.exe .\ecal_foxglove_bridge.py
+# python.exe .\coordinate_system_transformer.py
+# python.exe .\offender_detector.py
+# python.exe .\victim_detector.py
+# python.exe .\guardian_angel.py
+# python.exe .\guardian_angel.py
+
+
+thread_coordinate_system_transformer = threading.Thread(target=coordinate_system_transformer.run)
 
 thraed_offender_detector = threading.Thread(target=offender_detector.run)
 
 thraed_victim_detector = threading.Thread(target=victim_detector.run)
 
 thraed_guardian_angel = threading.Thread(target=guardian_angel.run)
+thraed_RemoteVehicle = threading.Thread(target=RemoteVehicle.run)
 
-thraed_coordinate_system_transformer = threading.Thread(target=guardian_angel.run)
 
-
-thraed_guardian_angel.start()
+thread_coordinate_system_transformer.start()
 thraed_offender_detector.start()
 thraed_victim_detector.start()
-thread_guardian_angel.start()
-thread_coordinate_system_transformer.start()
+thraed_guardian_angel.start()
+thraed_RemoteVehicle.start()
 
+print('Running...')
+while True:
+    print('Loop')
+    time.sleep(1)
 
-running = True
-while running:
-    time.sleep(0.1)
+print('Exit!')
